105e1175d93c3bb4b9e84c2482a17ff12325b4cd
lucas <lucas.zknz@gmail.com>
2026-04-13 19:50:57 -0300

    Initial commit: adicionando o codigo do jogo

---
+++ jogo.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const numeroSecreto = Math.floor(Math.random() * 101);
+let tentativas = 0;
+
+console.log("Jogo de Adivinhação ");
+console.log("Tente adivinhar o número entre 0 e 100");
+
+const jogar = () => {
+  rl.question("Qual o seu palpite? ", (resposta) => {
+    const palpite = parseInt(resposta);
+    tentativas++;
+
+    if (palpite === numeroSecreto) {
+      console.log(`Parabéns! Você acertou em ${tentativas} tentativas.`);
+      rl.close();
+    } else if (palpite < numeroSecreto) {
+      console.log("Muito baixo! Tente novamente.");
+      jogar();
+    } else {
+      console.log("Muito alto! Tente novamente.");
+      jogar();
+    }
+  });
+};
+
+jogar();
